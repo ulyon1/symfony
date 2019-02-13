3fa675630b222b1b01c1483b71d9b736aeaa747f
Boris Guéry <guery.b@gmail.com>
2019-02-13 17:04:43 +0100

    feat(conference):

--- src/Conferences/Conference.php
+++ src/Conferences/Conference.php
@@ -5,7 +5,7 @@ namespace Metinet\Conferences;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="Metinet\JobBoard\DoctrineJobRepository")
  */
 class Conference
 {
